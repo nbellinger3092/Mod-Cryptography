9e5dbc5d121150c2cb5a32afb638f5cae68ce30f
nbellinger3092 <44075657+nbellinger3092@users.noreply.github.com>
2019-04-09 12:59:51 -0400

    Add files via upload

--- ECDSA.py
+++ ECDSA.py
@@ -10,14 +10,15 @@ import random
 from datetime import datetime
 
 # G element of large prime order q
-P = [24,14]
-q = 3623
-A = 23
+P = [1,5]
+q = 9
+A = 3
+p = 13
 
 # Key creation
 s = 947
 
-V = FM.FastMultiple(s,P[0],P[1],q,A)
+V = FM.FastMultiple(s,P[0],P[1],p,A)
 print("V: " + str(V))
 
 # Signing
@@ -30,10 +31,10 @@ e = 11#(random.randint(1,q)) % q  # Random element #
 print("e: " + str(e))
 # Compute eG in E(Fp)
 
-eP = FM.FastMultiple(e,P[0],P[1],q,A)
+eP = FM.FastMultiple(e,P[0],P[1],p,A)
 print("eP: " + str(eP))
 S1 = eP[0] % q
-S2 = ((d + (s * eP[0])) * FI.FastInverse(e,q)) % q
+S2 = ((d + (s * eP[0])) * FI.FastInverse(e,p)) % q
 
 d_sig = S1,S2
 
@@ -43,16 +44,16 @@ print("d_sig: " + str(d_sig))
 
 # Verification
 
-V1 = (d * FI.FastInverse(S2,q)) % q
-V2 = (eP[0] * FI.FastInverse(S2,q)) % q
+V1 = (d * FI.FastInverse(S2,p)) % q
+V2 = (eP[0] * FI.FastInverse(S2,p)) % q
 print("V1: " + str(V1))
 print("V2: " + str(V2))
 
-x1,y1 = FM.FastMultiple(V1,P[0],P[1],q,A)
+x1,y1 = FM.FastMultiple(V1,P[0],P[1],p,A)
 print("x1: " + str(x1) + ", " + "y1: " + str(y1))
-x2,y2 = FM.FastMultiple(V2,V[0],V[1],q,A)
+x2,y2 = FM.FastMultiple(V2,V[0],V[1],p,A)
 print("x2: " + str(x2) + ", " + "y2: " + str(y2))
-temp = (EA.EllipticAdd(x1,y1,x2,y2,A,q))
+temp = (EA.EllipticAdd(x1,y1,x2,y2,A,p))
 print("temp: " + str(temp))
 verification = temp[0]
 
