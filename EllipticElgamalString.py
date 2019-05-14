d40056659c6e10cf79be456cd5c00a61af8e24ee
nbellinger3092 <44075657+nbellinger3092@users.noreply.github.com>
2019-05-14 12:32:02 -0400

    Add files via upload

--- EllipticElgamalString.py
+++ EllipticElgamalString.py
@@ -16,6 +16,8 @@ y1 = 730 #
 A = 14
 
 na = 947
+
+# Timer
 start = time.time()
 #################################
 #             Alice             #
@@ -33,15 +35,21 @@ Qa = FastMultiply.FastMultiple(na,x1,y1,p,A)
 
 message1 = "A+C"
 message2 = "B&D"
+
+# Initialize string variables to hold the encoded message1 and message2
 encodedMessage1 = ""
 encodedMessage2 = ""
 
+
+# For loops for converting the string messages 1 and 2 to ASCII characters 
+# and tacking them onto the variable encodedMessage1 and 2
 for i in range(len(message1)):
     encodedMessage1 = encodedMessage1 + str(ord(message1[i]))
 
 for i in range(len(message2)):
     encodedMessage2 = encodedMessage2 + str(ord(message2[i]))
 
+# Convert encodedMessage 1 and 2 back to integers for encryption
 m1 = int(encodedMessage1)
 m2 = int(encodedMessage2)
 
@@ -69,15 +77,16 @@ T = FastMultiply.FastMultiple(na,R[0],R[1],p,A)
 m1 = (FI.FastInverse(T[0],p) * c1) % p
 m2 = (FI.FastInverse(T[1],p) * c2) % p
 
+# Converts m1 and m2 to strings to be manipulated back to original text
 str_m1 = str(m1)
 str_m2 = str(m2)
 
-encodedMessage1 = ""
-encodedMessage2 = ""
-
+# Reinitialize m1 and m2 to hold final text value
 m1 = ""
 m2 = ""
 
+# Loops to convert the integer value of the string two indices at a time to capture the ASCII value
+# Converts that string value to integer, then to character, which is appended to m1 and m2 respectively
 for text in (str_m1[i:i+2] for i in range(0, len(str_m1), 2)):
     text = int(text)
     text = chr(text)
@@ -88,6 +97,8 @@ for text in (str_m2[i:i+2] for i in range(0, len(str_m2), 2)):
     text = chr(text)
     m2 = m2 + text
 
+
+# Print decoded message
 print("m1: " + str(m1))
 print("m2: " + str(m2))
 
